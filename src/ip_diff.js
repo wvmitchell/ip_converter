cf0c25485001a1d3018c37a9c1571a78678cb210
Robbie Jaeger <jaeger.rob@gmail.com>
2023-02-06 21:17:31 -0700

    Add solution

--- src/ip_diff.js
+++ src/ip_diff.js
@@ -1,1 +1,20 @@
-// Your code here
+function ipDiff(ip1, ip2) {
+  const ip1Total = ipTotal(ip1);
+  const ip2Total = ipTotal(ip2);
+
+  return Math.abs(ip1Total - ip2Total);
+}
+
+function ipTotal(ip) {
+  const multipliers = [256*256*256, 256*256, 256, 1];
+  
+  return ip.split('.').reduce((total, numberString, i) => {
+    const integer = parseInt(numberString);
+
+    total += integer*multipliers[i];
+
+    return total;
+  }, 0);
+}
+
+module.exports = { ipDiff };
