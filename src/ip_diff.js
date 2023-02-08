18dbda216a538c4a35952afcf73bba7f043cfcfc
Nick Teets <nickteets@gmail.com>
2023-02-08 08:46:54 -0700

    getting started, parsing ip

--- src/ip_diff.js
+++ src/ip_diff.js
@@ -1,1 +1,10 @@
-// Your code here
+const ipStringHandler = (ip) => ip.split('.').map(ip => parseInt(ip))
+
+module.exports = {
+    ipDiff(a, b) {
+        if (a === b) {
+            return 0
+        }
+        console.log(ipStringHandler(a))
+    }
+}
