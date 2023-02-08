8b99f796b5859edcfaa067f8ffea797bccf847e6
Nick Teets <nickteets@gmail.com>
2023-02-08 09:34:19 -0700

    splitting strings and figuring out which one is bigger

--- src/ip_diff.js
+++ src/ip_diff.js
@@ -1,10 +1,32 @@
 const ipStringHandler = (ip) => ip.split('.').map(ip => parseInt(ip))
 
+const findHigherIp = (a, b) => {
+    let higherIp
+    let i = 0
+    while(!higherIp && i < 4) {
+        if (a[i] > b[i]) {
+            higherIp = {hi: a, lo: b}
+        } else if (a[i] < b[i]) {
+            higherIp = {hi: b, lo: a}
+        } else if (a[i] === b[i]) {
+            i++
+        }
+    }
+    return higherIp
+}
+
+const calculateDiff = (ipObj) => {
+    const {hi, lo} = ipObj
+    return ()
+}
+
 module.exports = {
     ipDiff(a, b) {
         if (a === b) {
             return 0
         }
-        console.log(ipStringHandler(a))
+        const ipSort = findHigherIp(ipStringHandler(a), ipStringHandler(b))
+
+        console.log(ipSort)
     }
 }
